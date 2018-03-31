80d3514da42732b6c1755f1b59d448e03c22152b
Angela <johnson.angela.d@gmail.com>
2018-03-31 09:59:39 -0400

    some functionality; firebase not capturing

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # FBTrainSchedule
-Displays train schedule and allows users to add to schedule. Data stored in Firebase Database.
+
+#What it is; what it does:
+
+Train schedule application that incorporates Firebase to host arrival and departure data, providing up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+#Why the project is useful
+This app will allow users to create a schedule for their own traveling purposes.  If they know a little html/javascript, they can customize the schedule for their own purposes.
+
+#How users can get started with the project
+This project uses HTML, CSS, Javascript, and various sub-languages such as bootstrap, jquery. Be sure to include the links or libraries in your HTML <head></head>.
+
+You can (re)create this project by doing the following:
+
+#Outline
+Sign up for a free Firebase account (or another database service, if you'd like). View the tutorial(s) if you haven't used Firebase before.
+
+Use HTML to create divs to hold the different page tables:
+
+Page Title
+Empty information
+Train Schedule
+Add Train
+
+Use CSS to stylize the page.  I have used Bootstrap (3.3).
+
+Use Javascript/jQuery to give user the ability to manipulate the DOM and add new trains to the schedule.
+
+Use Moment.js to retrieve, manipulate, and calculate the time information.
+
+Use Firebase to collect the entries, so all users may view the same schedule.
+
+#Parameters considered
+This project was constructed with the following parameters in consideration:
+
+* When adding trains, administrators should be able to submit the following:
+    
+    * Train Name
+    
+    * Destination 
+    
+    * First Train Time -- in military time
+    
+    * Frequency -- in minutes
+  
+  * Code this app to calculate when the next train will arrive; this should be relative to the current time.
+  
+  * Users from many different machines must be able to view same train times.
+  
+  * Styling and theme are completely up to you. Get Creative!
+
+
+#Where users can get help with your project
+
+Get help with this project by using Google-Fu. :-)
+
+
+#Who maintains and contributes to the project
+
+This project was constructed and is maintained solely by AJ906.
+
+### Looking for a Challenge? ###
+
+* Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
+
+* Try adding `update` and `remove` buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
+
+* As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
+
+**Good Luck!**
---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+/* global firebase moment */
+// Steps to complete:
+
+// 1. Initialize Firebase
+// 2. Create button for adding new trains - then update the html + update the database
+// 3. Create a way to retrieve trains from the train database.
+// 4. Create a way to calculate the months worked. Using difference between start and current time.
+//    Then use moment.js formatting to set difference in months.
+// 5. Calculate Total billed
+
+// 1. Initialize Firebase
+
+var config = {
+  apiKey: "AIzaSyBcW0TMP-l76LSXoseTWTC0zzWVN41qu3A",
+  authDomain: "hw7-trainschedule-aj906.firebaseapp.com",
+  databaseURL: "https://hw7-trainschedule-aj906.firebaseio.com",
+  projectId: "hw7-trainschedule-aj906",
+  storageBucket: "hw7-trainschedule-aj906.appspot.com",
+  messagingSenderId: "1018254839108"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding trains
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var trnName = $("#train-name-input").val().trim();
+  var trnDest = $("#destination-input").val().trim();
+  var trnFirst = moment($("#first-train-input").val().trim(),"HH:mm").subract(10,"years").format("X");
+  var trnFreq = $("#frequency-input").val().trim();
+
+  // Creates local "ttrnorary" object for holding train data
+  var newTrn = {
+    name: trnName,
+    destination: trnDest,
+    first: trnFirst,
+    frequency: trnFreq
+  };
+
+  // Uploads train data to the database
+  database.ref().push(newTrn);
+
+  // Logs everything to console
+  console.log(newTrn.name);
+  console.log(newTrn.destination);
+  console.log(newTrn.first);
+  console.log(newTrn.frequency);
+
+  // Alert
+  alert("train successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trnName = childSnapshot.val().name;
+  var trnDest = childSnapshot.val().destination;
+  var trnFirst = childSnapshot.val().first;
+  var trnFreq = childSnapshot.val().frequency;
+
+  // train Info
+  console.log(trnName);
+  console.log(trnDest);
+  console.log(trnFirst);
+  console.log(trnFreq);
+
+  var remainder = moment().diff(moment.unix(trnFirst),"minutes")%frequency;
+  var minutesAway = frequency - remainder;
+  var nextArrival = moment().add(minutes,"m").format("hh:mm A");
+
+  console.log(remainder);
+  console.log(minutes);
+  // Prettify the train start
+  // var trnStartPretty = moment.unix(trnFirst).format("HH:mm");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  // var trnMonths = moment().diff(moment.unix(trnStart, "X"), "months");
+  // console.log(trnMonths);
+
+  // Calculate the total billed rate
+  // var trnBilled = trnMonths * trnRate;
+  // console.log(trnBilled);
+
+  // Add each train's data into the table
+  $("#trainSchedule-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDest + "</td><td>" +
+  trnFreq + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+});
+
+// Example Time Math
+// -----------------------------------------------------------------------------
+// Assume train start date of January 1, 2015
+// Assume current date is March 1, 2016
+
+// We know that this is 15 months.
+// Now we will create code in moment.js to confirm that any atttrnt we use meets this test case
